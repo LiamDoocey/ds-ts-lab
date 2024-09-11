1a19065b511862c57b3c066b79770e70ca0f26a6
Liam Doocey <ljdoocey@gmail.com>
2024-09-11 14:10:54 +0100

    Refactor - Add type declaration file; disable redundant console logs.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,8 +1,5 @@
-interface Friend {
-    name: string;
-    phone: string;
-    age: number;
-}
+import { Friend, Colleague, ColleagueHistory } from "./myTypes";
+
 
 const friend1: Friend = {
     name: 'Thomas',
@@ -16,24 +13,11 @@ const friend2: Friend = {
     age: 28
 };
 
-const friends: Friend[]  = [friend1, friend2];
-console.log(friends[1]);
+export const friends: Friend[]  = [friend1, friend2];
+// console.log(friends[1]);
 
 // --------------------------------------------------
 
-interface Colleague {
-    name: string;
-    department: string;
-    contact: {
-        extenstion: number;
-        email: string;
-    };
-}
-
-interface ColleagueHistory {
-    current: Colleague[],
-    former: Colleague[]
-  }
 
 const colleague1: Colleague = {
     name: 'Mary Jane',
@@ -62,9 +46,9 @@ const colleague3: Colleague = {
     }
 };
 
-const colleagues: ColleagueHistory = {
+export const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: []
 };
 
-console.log(colleagues.current[2]);
+// console.log(colleagues.current[2]);
---
+++ src/myTypes.d.ts
@@ -0,0 +1,19 @@
+export interface Friend {
+    name: string;
+    phone: string;
+    age: number
+}
+
+export interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        extenstion: number;
+        email: string;
+    } 
+  }
+
+export interface ColleagueHistory {
+  current: Colleague[],
+  former: Colleague[]
+}
